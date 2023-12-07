0cd450ef54f5ead45949097b3707cf4b8458591e
aman.thakur <aman.thakur>
2023-12-07 17:43:32 +0530

    added axios, sessions

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session, redirect, url_for
+import os
+from datetime import timedelta 
 
-from auth.login_blueprint import login_blueprint
+from auth.login_blueprint import login_blueprint,user_logged_in
 
 app = Flask(__name__)
 app.register_blueprint(login_blueprint)
-
 
 
 @app.get("/")
@@ -17,3 +18,5 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 
+app.secret_key=os.urandom(24)
+app.permanent_session_lifetime=timedelta(seconds=10)
--- auth/login_blueprint.py
+++ auth/login_blueprint.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, render_template, make_response,jsonify
+from flask import Blueprint, request, render_template, make_response,jsonify,session
 import jwt
 import secrets
+import datetime
 login_blueprint=Blueprint("login_blueprint",__name__)
 
 
@@ -14,14 +15,37 @@ def valid_login(username,password):
         created_token=create_token(username)
         return created_token
     else:
-        return None
+        return 
     
+def user_logged_in():
+    if "session" in session:
+        print(True)
+        return True
+    else:
+        print(False)
+        return False
+    
+def create_session_for_user(token):
+    session["session"]=token
 
 @login_blueprint.post("/login")
 def login():
-    isValidLogin=valid_login(request.form["name_val"],request.form["pass_val"])
-    if isValidLogin is not None:
-        return jsonify(isValidLogin),200
+    data=request.json
+    username=data.get("username")
+    password=data.get("password")
+    
+    # username=request.form["name_val"]
+    # password=request.form["pass_val"]
+    
+    #  # check session exists
+    is_user_logged_in=user_logged_in()
+    
+    if is_user_logged_in == False:
+        token=valid_login(username,password)
+        
+        create_session_for_user(token)
+        
+        return "Done"
     else:
-        response=make_response("Wrong Password",401)
+        response=make_response("user logged in ")
         return response
